89cbfd7cefab5056d2189b3337081f6a57a15a65
Aynaz <jkz1713x2@gmail.com>
2025-08-09 16:43:59 +0300

    add header logo footer

--- index.php
+++ index.php
@@ -1,50 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/fontello.css">
-    <link rel="stylesheet" href="style.css">
-    <title>My theme</title>
-    <?php wp_head(); ?>
-</head>
-
-<body>
-    <header class="header">
-        <div class="container">
-            <nav class="menu__desktop">
-                <ul class="menu">
-                    <li class="menu__item active"><a href="#">Home</a></li>
-                    <li class="menu__item"><a href="#about">About Us</a></li>
-                    <li class="menu__item"><a href="#team">Team</a></li>
-                    <li class="menu__item"><a href="#"><img src="img/logo.png" alt="" class="logo"></a></li>
-                    <li class="menu__item"><a href="#provide">Services</a></li>
-                    <li class="menu__item"><a href="#">Blog</a></li>
-                    <li class="menu__item"><a href="#contact">Contact Us</a></li>
-                </ul>
-            </nav>
-            <nav class="menu__mobile">
-                <div class="menu__inner">
-                    <a href="#"><img src="img/logo.png" alt="" class="logo"></a>
-                    <div class="menu__burger"><span>toggle menu</span></div>
-                </div>
-                <ul class="menu">         
-                    <li class="menu__item active"><a href="#">Home</a></li>
-                    <li class="menu__item"><a href="#about">About Us</a></li>
-                    <li class="menu__item"><a href="#team">Team</a></li>
-                    <li class="menu__item"><a href="#provide">Services</a></li>
-                    <li class="menu__item"><a href="#">Blog</a></li>
-                    <li class="menu__item"><a href="#contact">Contact Us</a></li>
-                </ul>
-            </nav>
-            <div class="header__content">
-                <h1 class="header__title">We build it with passion</h1>
-                <p class="header__text">Just to be clear, we do this for fun not for you, just kidding.</p>
-                <a href="" class="header__button">READ MORE</a>
-            </div>
-        </div>
-    </header>
+<?php get_header(); ?>
     <div class="about" id="about">
         <div class="container">
             <div class="about__inner">
@@ -167,17 +121,4 @@
             </div>
         </div>
     </div>
-    <div class="footer">
-        <div class="container">
-            <div class="footer__text">
-                Copyright &copy; Kenan Hamidic. All rights reserved.
-            </div>
-        </div>
-    </div>
-
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/main.js"></script>
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+<?php get_footer(); ?>
